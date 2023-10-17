e3cca7776ca402a91f034b8808be94280344749e
jb-2015 <juan_matias_becerra@hotmail.com>
2023-10-17 20:51:28 -0300

    modificaciones en el cliente

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,23 @@
 const btn_personal= document.getElementById('entrar-personal')
+const btn_buscarP= document.getElementById('buscarPaciente')
+
+
 
 btn_personal.addEventListener('click',()=>{
     window.location.assign('/page-administrativo')
+})
+
+btn_buscarP.addEventListener('click',()=>{
+    let valor = document.getElementById('dni-paciente-search')   
+
+    fetch(`persona/dni/:${valor.value}`,{
+       
+    })
+    .then(res=>res.json())
+    .then(d=>{
+
+    })
+    .catch(error =>{
+        console.log("Error: "+error)
+    })
 })
